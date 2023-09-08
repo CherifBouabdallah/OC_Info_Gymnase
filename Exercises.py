a2a23a20eb5716be64c1d1a6111a264fcfc8e170
Cherif Bouabdallah GYYV <chrif.bouabdllah@gmail.com>
2023-09-08 08:26:39 +0200

    test

---
+++ Exercises.py
@@ -0,0 +1,50 @@
+import time 
+import math
+
+'''
+
+r = 5
+pi = 3.14159
+aire= 2 * pi * r
+
+bool(aire)
+print(aire)
+print(type(aire))
+
+
+
+mon_age =100 # mon_age est 
+ma_taille = 1.95 # est un 
+mon_nom = "Dromard" # est
+vrai= True # vrai est un booléen
+
+print(type(mon_age)) # La sortie va être <class int> 
+print(type(ma_taille + 10)) # La sortie va être <class float> 
+print(type(mon_nom)) # La sortie va être <class str> 
+print(type(vrai)) # La sortie va être <class bool>
+
+
+
+
+number = (input('Donne un nombre à 4 chiffres '))
+number_bb = (input('Donne un nombre à 4 chiffres '))
+
+
+print(int(number) - 1)
+print(int(number) + 1)
+
+print(str(number)[0])
+print(str(number)[1])
+print(str(number)[2])
+print(str(number)[3])
+
+print(number_bb // number)
+print(number_bb // number)
+
+'''
+
+
+L=["a", "b", "c", "d", "e", "f", "g"] 
+
+for i in range(6, 0, 1):
+    print(L[i])
---
+++ Year_detector.py
@@ -0,0 +1,15 @@
+day = int(input("give me the day of the month in numbers : "))
+month = int(input("give me the the month in numbers : "))
+year = int(input("give me the year : "))
+
+myday = int(input("give me the day of the month that you were born in numbers : "))
+mymonth = int(input("give me the month that you were born in numbers : "))
+myyear = int(input("give me the year that you were born : "))
+
+if mymonth < month:
+    age = myyear - month
+
+
+
+
+print("You are "+(age)+ "years old")
