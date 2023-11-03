1d04a42934799c9ceb82ca949d1738e0fb36e077
Cherif Bouabdallah GYYV <chrif.bouabdllah@gmail.com>
2023-11-03 09:32:32 +0100

    TP2 Suite Nov3 2023

--- TP/TP2/TP2_Cryptage.py
+++ TP/TP2/TP2_Cryptage.py
@@ -1,20 +1,18 @@
-mot = 'Hello'
+mot = 'Hello World'
 
-fichier = open('/Users/pi73xdo/Desktop/Cherif_Info/TP/Hooverphonic.txt', "r")
+fichier = open('/Users/pi73xdo/Desktop/Cherif_Info/TP/TP2/Hooverphonic.txt', "r")
 text_file = fichier.read()
-print(text_file)
 
-fichier2 = open('/Users/pi73xdo/Desktop/Cherif_Info/TP/Madaboutyou.txt', "w+")
-print(text_file, file=fichier2)
+fichier2 = open('/Users/pi73xdo/Desktop/Cherif_Info/TP/TP2/Madaboutyou.txt', "a")
+
+mystere = open('/Users/pi73xdo/Desktop/Cherif_Info/TP/TP2/mystere1.txt', "r")
+mystere_file = mystere.read()
 
 def Crypt(mot):
     space = ""
     for i in mot:
         space += str(ord(i)) + " "
     return space
-
-crypted = (Crypt(mot))
-print(crypted)
 
 def Decry(crypted):
     
@@ -34,6 +32,60 @@ def Decry(crypted):
 
     return final    
     
-    
-print(Decry(crypted))
 
+word_cry = Crypt(mot)
+print(word_cry)
+#print(Decry(mystere_file))
+
+
+#encrypted = Crypt(text_file)
+#print(encrypted, file=fichier3)
+#fichier3.close()
+
+def NewName(file_name):
+    new_name = ""
+    for i in range(len(file_name)):
+        if file_name[i] != ".":
+            new_name += file_name[i]
+        else:
+            new_name += "_encrypted."
+
+    return new_name
+
+def CryptAndRename(file_name):
+    encrypted = Crypt(text_file)
+    encrypted_file = open('/Users/pi73xdo/Desktop/Cherif_Info/TP/TP2/' + NewName(file_name), "w")
+    print(encrypted, file=(encrypted_file))
+    encrypted_file.close()
+
+
+def CryptASCII(word, key):
+    n=0
+    final = ""
+    space = ""
+    for i in word:
+        space += str(ord(i) + key) + " "
+
+    while n < len(space):
+            temp_numbers = ""
+
+            while n < len(space) and space[n] != " ":
+                temp_numbers += space[n]
+                n += 1
+
+            decry = chr(int(temp_numbers))
+            final += decry
+            n += 1
+
+    return final
+
+def DecryptASCII(word, key):
+    n=0
+    final = ""
+    space = ""
+    for i in word:
+    
+
+
+print(CryptASCII("Hello World", 8))
+print(DecryptASCII("Pmttw%_evi%`{sv", 8))
