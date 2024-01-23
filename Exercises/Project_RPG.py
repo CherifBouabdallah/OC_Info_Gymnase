26afc40a1ebebf1b3e6218f5146070820d30afd6
Cherif Bouabdallah GYYV <chrif.bouabdllah@gmail.com>
2024-01-23 11:29:43 +0100

    23 jan 2024

--- Exercises/Project_RPG.py
+++ Exercises/Project_RPG.py
@@ -7,7 +7,7 @@ class Character:
     dico_weapon = {}
     dico_weapon_power = {}
 
-    def __init__(self, name, age = 18, strength = 1, weapon = 'Knife', typ='Civilian', mana=10, life = 100):
+    def __init__(self, name, age = 18, strength = 1, weapon = 'Knife', typ='Civilian', mana=10, life = 500):
         self.name = name
         self.age = age
         self.strength = strength
@@ -54,10 +54,10 @@ class Character:
 
     def hit(self, victim):
         if self.weapon in Character.dico_weapon_power:
-            victim.life -= Character.dico_weapon_power[self.weapon]
+            victim.life -= Character.dico_weapon_power[self.weapon] + self.power
             self.mana -= 1
         else:
-            victim.life -= self.stength
+            victim.life -= self.power
 
     def fight(self, oppo):
         while self.life > 0 and oppo.life > 0:
@@ -66,12 +66,45 @@ class Character:
             print(self.life, oppo.life)
         if self.life > oppo.life:
             print(self.name, 'wins with', self.life, 'points of life using', self.weapon, 'against', oppo.name, 'that was using', oppo.weapon)
+            del(self)
         else:
             print(oppo.name, 'wins with', oppo.life, 'points of life using', oppo.weapon, 'against', self.name, 'that was using', self.weapon)
+            del(oppo)
+
 
     def reset_life(self):
         self.life = 100
+
+    @property
+    def power(self):
+        return self.mana + self.age
+    
+    @property
+    def intro(self):
+        return "Your character's name is "+self.name+ ' and he is ' + str(self.age)+' years old. His strength is ' + str(self.strength)+' and he holds some ' + self.weapon+ '. As a ' + self.typ+ ', he has '+ str(self.mana) + ' mana and ' + str(self.life) + ' points of life.'
+
         
+    def __del__(self):
+        print(self.name, 'is dead, haha.')
+    
+    def __repr__(self):
+        return self.name+" is "+str(self.age)+" years old."
+    
+    def __str__(self):
+        return self.name+ ' is '+str(self.age)+" and he is "+self.typ
+
+
+
+
+class Civillian(Character):
+    def __init__(self, name, age = 18, strength = 1, weapon = 'Knife', typ='Civilian', mana=10, life = 500, spell='wind'):
+        super().__init__(name, age, strength, weapon, typ ,mana, life)
+        self.spell = spell
+
+    def __str__(self):
+        return self.name+" is a civillian"
+
+
 class Place:
     nbr = 0
     dico_typ = {}
@@ -98,22 +131,24 @@ class Place:
 def setting_teller(location):
     print('This place is named', location.city,'it is located in the state of', location.province, 'in a country named', location.country)
 
+    def __del__(self):
+        print("No more of this place")
+    
+    def __repr__(self):
+        return self.city + self.province + self.country
+    
+    def __str__(self):
+        return self.city + self.province + self.country + self.typ
+
+
 
 me = Character('V', 19, 5, 'Blades', 'Merc')
 me2 = Character('W', typ='Merc', weapon='Gun')
-me3 = Character('X', 5)
+me3 = Character('X', 50)
 me4 = Character('Y', strength=6, weapon='Gun')
 
 location = Place('Night City', 'New California', 'NUSA')
 
-
-setting_teller(location)
-print(Character.dico_typ)
-print(Character.dico_weapon)
-print(Character.dico_weapon_power)
-print(me.life)
-print(me2.life)
 #me.fight(me2)
-#me.fight(me3)
-print(Place.dico_typ_power)
-print(Place.dico_typ)
+me.fight(me3)
+
